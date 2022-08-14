be2026743be36b1d7a01641dd981c2b6c0a90f10
Maxim Savin <maxim.savin@gmail.com>
2022-08-14 18:57:39 +0700

    fix vertical

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.scss'
 // import logo from '../public/img/logo.png'
 // import typescriptLogo from './typescript.svg'
 // import { setupCounter } from './counter'
